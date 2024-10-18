289b267059e0b2ddb0c52ebf02473d0f598f6466
Zain <zainhussaini9898@gmail.com>
2024-10-19 04:22:34 +0530

    ticket page email chnaged

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -117,7 +117,7 @@ const TicketPage = () => {
                   d="M16 7L12 11M8 7L12 11M21 6C21 5.44772 20.5523 5 20 5H4C3.44772 5 3 5.44772 3 6V18C3 18.5523 3.44772 19 4 19H20C20.5523 19 21 18.5523 21 18V6Z"
                 />
               </svg>
-              <p className="text-base text-gray-700">info@thekevinchoe.com</p>
+              <p className="text-base text-gray-700">Nitapatel410@gmail.com</p>
             </div>
           </div>
         </div>
