ecc4b387f53355e2d6ca23d1b170df058a6e88c0
Zain <zainhussaini9898@gmail.com>
2024-10-22 03:08:56 +0530

    title change

--- app/page.tsx
+++ app/page.tsx
@@ -34,9 +34,9 @@ export default function Home() {
           </video>
 
           <div className="relative flex flex-col items-center justify-center h-screen md:gap-[8vh] gap-[14vh]">
-            <p className="text-white md:text-7xl lg:text-7xl text-4xl text-center px-12 md:px-6">
-              PREFORECLOSURE AND MINDSET WORKSHOP
-            </p>
+          <p className="text-white md:text-7xl lg:text-7xl text-4xl text-center px-12 md:px-6">
+            <span className="text-[#63AB38]">PREFORECLOSURE</span> AND <span className="text-[#63AB38]">MINDSET</span> WORKSHOP
+          </p>
             <p className="text-md md:text-lg lg:text-2xl font-semibold text-center text-white">
               Learn how to make $50K a month by mastering preforeclosures and
               unlocking the power of your mindset.
