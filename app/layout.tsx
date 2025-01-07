feb1ed49e2d7732c16045afb2313a0fa7f6fabae
Zain <zainhussaini9898@gmail.com>
2025-01-08 04:40:11 +0530

    meta data updated 9

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,22 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+
 import { Footer } from "@/components/Footer";
 import NavbarWrapper from "@/components/NavbarWrapper";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("http://goldminingforpreforeclosures.com/"),
-  title: "Preforeclosure Workshop",
-  description: "A Workshop By Nita Patel",
-  openGraph: {
-    title: "Preforeclosure Workshop",
-    description: "A Workshop By Nita Patel",
-    images: [
-      {
-        url: "/logo.png",
-        width: 1200,
-        height: 630,
-        alt: "A Workshop By Nita Patel",
-      },
-    ],
-    type: "website",
-    url: "/logo.png",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Preforeclosure Workshop",
-    description: "A Workshop By Nita Patel",
-    images: ["/logo.png"],
-  },
-  icons: {
-    icon: "/favicon.ico", // Add this line to link to your favicon
-  },
+  title: "PREFORECLOSURE WORKSHOP ",
+  description: "A workshop  by Nita Patel",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" /> {/* Link to the favicon */}
-      </head>
       <body className="">
         <div className="relative z-50">
           <NavbarWrapper /> {/* Use the client-side wrapper component */}
