8dd89537596daffddfbca73fc4f2548c00c05cf3
Zain <zainhussaini9898@gmail.com>
2025-01-08 04:11:01 +0530

    meta data updated 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,33 @@ import { Footer } from "@/components/Footer";
 import NavbarWrapper from "@/components/NavbarWrapper";
 
 export const metadata: Metadata = {
-  title: "PREFORECLOSURE WORKSHOP ",
-  description: "A workshop  by Nita Patel",
+  metadataBase: new URL("http://goldminingforpreforeclosures.com/"),
+  title: "Preforeclosure Workshop",
+  description:
+    "A Workshop By Nita Patel",
+  openGraph: {
+    title: "Preforeclosure Workshop",
+    description:
+      "A Workshop By Nita Patel",
+    images: [
+      {
+        url: "/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "A Workshop By Nita Patel",
+      },
+    ],
+    type: "website",
+    url: "/logo.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Preforeclosure Workshop",
+    description:
+      "A Workshop By Nita Patel",
+    images: ["/logo.png"],
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
