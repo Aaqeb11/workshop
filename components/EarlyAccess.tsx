820366fe1c5e0008f0e23c6ee248fe6a03cfac65
Zain <zainhussaini9898@gmail.com>
2025-01-08 03:36:11 +0530

    meta data updated 5

--- components/EarlyAccess.tsx
+++ components/EarlyAccess.tsx
@@ -99,7 +99,7 @@ export const EarlyAccess = () => {
       <div className="flex flex-col items-center flex-wrap lg:w-[30%] gap-6">
         <p className="text-sm font-bold">ARE YOU READY?</p>
         <p className="md:text-4xl text-2xl text-center font-bold">
-          Get Early Access To Our 3 Day Workshop
+          Get Early Access To Our 2 Day Workshop
         </p>
         <Link href="/tickets">
           <button className="text-black hover:bg-black hover:text-white focus:ring-4 focus:outline-none focus:ring-green-300 outline outline-black font-medium rounded-3xl text-md md:text-xl md:px-8 px-6 py-4 text-center flex items-center md:gap-4 gap-3 group duration-300 hover:outline-white">
