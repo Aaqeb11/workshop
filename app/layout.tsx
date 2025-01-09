974233e11b38b19e8f023d735e7bea05dc77d030
Aaqeb11 <aaqebahmed@gmail.com>
2025-01-10 00:03:38 +0530

    feat: added meta data with black background logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,10 +6,32 @@ import { Footer } from "@/components/Footer";
 import NavbarWrapper from "@/components/NavbarWrapper";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://creative-funding.vercel.app"),
   title: "PREFORECLOSURE WORKSHOP ",
   description: "A workshop  by Nita Patel",
+  openGraph: {
+    title: "Creative Funding | Flexible & Reliable Loan Solutions",
+    description:
+      "Experience hassle-free lending with Creative Funding. We offer various types of loans with an easy process, trustworthy service, and round-the-clock availability. Your reliable partner in financial solutions.",
+    images: [
+      {
+        url: "/logo_black_background.png",
+        width: 1200,
+        height: 630,
+        alt: "Creative Funding Logo",
+      },
+    ],
+    type: "website",
+    url: "/logo_black_background.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Creative Funding | Simple & Trusted Loan Solutions",
+    description:
+      "Looking for flexible loan options? Creative Funding offers various types of loans with an easy process, trustworthy service, and 24/7 support. Your path to reliable financial solutions starts here.",
+    images: ["/logo_black_background.png"],
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
