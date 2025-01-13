c130682f93575bf3d5d74773eb9c5605df3e1af2
Zain <zainhussaini9898@gmail.com>
2025-01-14 02:46:48 +0530

    form connected to GHL

--- components/AppointmentForm.tsx
+++ components/AppointmentForm.tsx
@@ -1,133 +1,47 @@
 "use client";
-
 import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import emailjs from "emailjs-com";
-import { FaCircleArrowRight } from "react-icons/fa6";
-
-type FormFields = {
-  name: string;
-  number: string;
-  email: string;
-  description: string;
-};
 
 const AppointmentForm = () => {
-  const template = process.env.NEXT_PUBLIC_TEMPLATE_ID;
-  const service = process.env.NEXT_PUBLIC_SERVICE_ID;
-  const key = process.env.NEXT_PUBLIC_USER_ID;
-
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    // Load the form embed script when the component mounts
+    const script = document.createElement("script");
+    script.src = "https://link.msgsndr.com/js/form_embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      // Clean up the script on component unmount
+      document.body.removeChild(script);
+    };
   }, []);
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      name: "",
-      number: "",
-      email: "",
-      description: "",
-    },
-  });
-
-  const onSubmit = (data: FormFields) => {
-    emailjs
-      .send(service!, template!, data, key)
-      .then((response) => {
-        reset();
-        console.log("Form data:", data);
-        toast.success("Form Submitted Successfully!");
-      })
-      .catch((error) => {
-        console.error("FAILED...", error);
-        toast.error("Form Submission Failed!");
-      });
-  };
-
   return (
-    <>
-      <div className="bg-black md:p-8 rounded-xl shadow-md">
-        <form
-          className="flex flex-col space-y-4"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <input
-            {...register("name", { required: "Name is required" })}
-            type="text"
-            placeholder="Name"
-            className="p-2 border border-gray-500 bg-gray-800 text-white rounded-md"
-          />
-          {errors.name && (
-            <div className="text-red-500">{errors.name.message}</div>
-          )}
-
-          <input
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: "Invalid email address",
-              },
-            })}
-            type="text"
-            placeholder="Email"
-            className="p-2 border border-gray-500 bg-gray-800 text-white rounded-md"
-          />
-          {errors.email && (
-            <div className="text-red-500">{errors.email.message}</div>
-          )}
-
-          <input
-            {...register("number", {
-              required: "Phone number is required",
-              minLength: {
-                value: 10,
-                message: "Phone number must be at least 10 digits",
-              },
-            })}
-            type="text"
-            placeholder="Phone Number"
-            className="p-2 border border-gray-500 bg-gray-800 text-white rounded-md"
-          />
-          {errors.number && (
-            <div className="text-red-500">{errors.number.message}</div>
-          )}
-
-          <textarea
-            {...register("description", {
-              required: "Description is required",
-              minLength: {
-                value: 5,
-                message: "Description must be at least 5 characters long",
-              },
-            })}
-            placeholder="Message"
-            className="p-2 border border-gray-500 bg-gray-800 text-white rounded-md"
-            rows={4}
-          />
-          {errors.description && (
-            <div className="text-red-500">{errors.description.message}</div>
-          )}
-
-          <button
-            type="submit"
-            className="item-center justify-center h-[50px]  text-white hover:bg-[#63AB38] hover:text-black focus:ring-4 focus:outline-none focus:ring-green-300 outline outline-white font-medium rounded-3xl text-md md:text-xl md:px-8 px-6 py-4 text-center flex items-center md:gap-4 gap-3 group duration-300"
-          >
-            Submit
-            <FaCircleArrowRight className="md:text-lg text-[#63AB38] group-hover:text-black" />
-          </button>
-        </form>
-        <ToastContainer />
-      </div>
-    </>
+    <div className="w-full h-auto my-10">
+      <iframe
+        src="https://api.leadconnectorhq.com/widget/form/D1wWq6kIkz4FBK253VyD"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+          borderRadius: "3px",
+        }}
+        id="inline-D1wWq6kIkz4FBK253VyD" 
+        data-layout="{'id':'INLINE'}"
+        data-trigger-type="alwaysShow"
+        data-trigger-value=""
+        data-activation-type="alwaysActivated"
+        data-activation-value=""
+        data-deactivation-type="neverDeactivate"
+        data-deactivation-value=""
+        data-form-name="Form 0"
+        data-height="511"
+        data-layout-iframe-id="inline-D1wWq6kIkz4FBK253VyD"
+        data-form-id="D1wWq6kIkz4FBK253VyD"
+        title="Form 0"
+      ></iframe>
+    </div>
   );
 };
 
 export default AppointmentForm;
+
